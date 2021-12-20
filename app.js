b11f90aa955c2ea3fa8ad6f836c82488d08668b5
risheetnair <risheetnair@outlook.com>
2021-12-19 20:16:09 -0500

    added button for check mark and deleting todo item

--- app.js
+++ app.js
@@ -18,4 +18,14 @@ function addTodo(event){
     newTodo.innerText = 'hey';
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
+    //Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    //Check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('complete-btn');
+    todoDiv.appendChild(trashButton);
 }
