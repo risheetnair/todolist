48da3980570b1c23993e9e16ae3198ffb47fb3ab
risheetnair <risheetnair@outlook.com>
2021-12-19 21:54:32 -0500

    cleared input value after adding list item

--- app.js
+++ app.js
@@ -30,4 +30,6 @@ function addTodo(event){
     todoDiv.appendChild(trashButton);
     //Append to list
     todoList.appendChild(todoDiv);
+    //Clear todo input value
+    todoInput.value = "";
 }
