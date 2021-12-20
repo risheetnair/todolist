5849705cf40b96b68110d808230d50b49a3f84d6
risheetnair <risheetnair@outlook.com>
2021-12-19 22:04:12 -0500

    added check for todo completion

--- app.js
+++ app.js
@@ -42,4 +42,10 @@ function deleteCheck(event){
         const todo = item.parentElement;
         todo.remove();
     }
+
+    //Check
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
